e1c7f4f0bc16b3aac47d565d34a9c05f1ed06f47
Phelete <foxemaine@gmail.com>
2024-10-28 12:41:57 +0100

    28.10.2024 Language button fixed

--- assets/js/language-switcher.js
+++ assets/js/language-switcher.js
@@ -1,9 +1,7 @@
 function setLanguage(language) {
     const urlParts = window.location.pathname.split('/');
-    if (urlParts[1] === 'cs' || urlParts[1] === 'en' || urlParts[1] === 'ru') {
-        urlParts[1] = language;
-    } else {
-        urlParts.unshift(language);
-    }
+    const lastPartIndex = urlParts.length - 2;
+    urlParts[lastPartIndex] = ['cs', 'ru'].indexOf(language) > -1 ? language : 'en';
+    console.log(urlParts);
     window.location.pathname = urlParts.join('/');
 }
