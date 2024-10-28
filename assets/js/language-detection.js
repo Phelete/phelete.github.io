051246696a265ea96f8508ba18574ef40ca42aee
Phelete <foxemaine@gmail.com>
2024-10-28 12:47:01 +0100

    28.10.2024 Minor fixes

--- assets/js/language-detection.js
+++ assets/js/language-detection.js
@@ -2,6 +2,6 @@ function detectLanguage() {
   return navigator.language || navigator.userLanguage;
 }
 let detectedLanguage = detectLanguage();
-let acceptedLanguages = ['cs', 'ru'];
+let acceptedLanguages = ['cs', 'ru', 'en'];
 let targetLanguage = acceptedLanguages.find(lang => detectedLanguage.startsWith(lang)) || 'en';
 window.location.replace(targetLanguage);
--- assets/js/language-switcher.js
+++ assets/js/language-switcher.js
@@ -1,7 +1,7 @@
 function setLanguage(language) {
     const urlParts = window.location.pathname.split('/');
     const lastPartIndex = urlParts.length - 2;
-    urlParts[lastPartIndex] = ['cs', 'ru'].indexOf(language) > -1 ? language : 'en';
+    urlParts[lastPartIndex] = ['cs', 'ru', 'en'].indexOf(language) > -1 ? language : 'en';
     console.log(urlParts);
     window.location.pathname = urlParts.join('/');
 }
